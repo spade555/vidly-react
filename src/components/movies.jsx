4c14a57ab6c14d15f28bcbd5f9b974c0eec720d4
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-08-28 17:39:05 +0200

    Implemented desc sorting.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -44,7 +44,14 @@ class Movies extends Component {
   };
 
   handleSort = path => {
-    this.setState({ sortColumn: { path, order: "asc" } });
+    const sortColumn = { ...this.state.sortColumn };
+    if (sortColumn.path === path) {
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    } else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.setState({ sortColumn });
   };
 
   render() {
