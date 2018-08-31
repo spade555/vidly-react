0d433163c7e7a2354145ddf87023892dac9e3035
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-08-31 17:32:47 +0200

    Ahndling of input fields of login form implemented.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = this.username.current.value;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              name="username"
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
