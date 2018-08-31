65e9e1e0c1a6f5645fd9eac77dfaaecd8e9d50f5
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-08-31 14:18:53 +0200

    Routes added to the App with the new simple components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/movies";
-import Navbar from "./components/common/navbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import Navbar from "./components/navbar";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 
 class App extends Component {
@@ -9,7 +14,15 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
         <main className="container">
-          <Movies />
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
         </main>
       </React.Fragment>
     );
