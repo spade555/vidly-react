9e9cfc53e2451dd7548adc54e4f65c1c0888083e
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-11 11:48:00 +0200

    Typo fixed.

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ name, label, value, onChange }) => {
   return (
     <div className="form-group">
-      <label htmlFor={name}>{label}}</label>
+      <label htmlFor={name}>{label}</label>
       <input
         value={value}
         onChange={onChange}
