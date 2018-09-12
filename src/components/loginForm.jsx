47b379b50787eadaf6eb0821450ba2466d128b3a
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-12 16:55:33 +0200

    Refactoring the form related codes.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Input from "./common/input";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
@@ -23,28 +22,13 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
