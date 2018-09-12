27724a4a26f51cd15c5fa3a5ed351663c0348c9f
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-12 17:04:35 +0200

    RegisterForm and its root added.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/navbar";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 class App extends Component {
@@ -17,6 +18,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = {};
+  render() {
+    return <h1>Register</h1>;
+  }
+}
+
+export default RegisterForm;
