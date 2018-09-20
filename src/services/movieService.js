907c4ce4d0398722f5ff4fabec702e175035283d
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-20 15:26:29 +0200

    Implemented real MovieService.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,10 +3,27 @@ import { apiURL } from "../config.json";
 
 const apiEndpoint = apiURL + "/movies";
 
-export async function getMovies() {
-  return await http.get(apiEndpoint);
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
 }
 
-export async function deleteMovie(movieId) {
-  return await http.delete(apiEndpoint + "/" + movieId);
+export function getMovies() {
+  return http.get(apiEndpoint);
+}
+
+export function deleteMovie(movieId) {
+  return http.delete(movieUrl(movieId));
+}
+
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
+
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+  return http.post(apiEndpoint, movie);
 }
