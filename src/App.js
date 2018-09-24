22c7b59279966cc1f35e4759f99fe57b6742a1c0
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-24 16:33:42 +0200

    Logout implemented.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/navbar";
 import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/logout";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -32,6 +33,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/register" component={RegisterForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
