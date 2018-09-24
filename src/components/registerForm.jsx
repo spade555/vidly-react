945015df4544a5ad2a95a7be88d52b0d16545129
Gergely Sandor Szabo <gergely_sandor_szabo@epam.com>
2018-09-24 15:39:43 +0200

    Instant login after registration.

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -25,7 +25,9 @@ class RegisterForm extends Form {
 
   doSubmit = async () => {
     try {
-      await UserService.register(this.state.data);
+      const response = await UserService.register(this.state.data);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
